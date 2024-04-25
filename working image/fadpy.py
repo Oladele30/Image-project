c62ef56a743ef0b7d4ee1881689e9e9aca873ee5
Usman Fadil <usmanfadiloladele@gmail.com>
2024-04-25 17:49:41 +0100

    first commit

---
+++ working image/fadpy.py
@@ -0,0 +1,23 @@
+# course_title = "AEF"
+# course_name = "miceoeconomic"
+# course_name2 = "macroeconomic"
+# name = course_title + " " + course_name + "\n" + course_name2
+# print(name)
+
+# course_title = "AEF"
+# course_name = "miceoeconomic"
+# course_name2 = "macroeconomic"
+# name = course_title + " " + course_name + " " + course_name2
+# print(name)
+
+# name = "Alm creative"
+# age = "19"
+# role = "python guru"
+# info = "my name is " + name + " " + "my age is" + " "+  age + " " + "a " + role
+# print(info)
+
+# name = "Alm creative"
+# age = "19"
+# role = "python guru"
+# info = f"my name is {name} my age is {age} my role is {role}"
+# print(info)
